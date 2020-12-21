29a33323f443b4f4b4356b342529a99d994e68ef
PRITAM RAJ <31183383+rpritam010@users.noreply.github.com>
2020-12-21 19:08:39 +0530

    Create SocialdemoApplicationTests.java

---
+++ socialdemo/src/test/java/com/socialdemo/socialdemo/SocialdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialdemo.socialdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
