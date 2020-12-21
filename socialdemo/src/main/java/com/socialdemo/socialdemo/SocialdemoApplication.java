38d3a4713feb69a964860938889977728575e69c
PRITAM RAJ <31183383+rpritam010@users.noreply.github.com>
2020-12-21 18:49:24 +0530

    Update SocialdemoApplication.java

--- socialdemo/src/main/java/com/socialdemo/socialdemo/SocialdemoApplication.java
+++ socialdemo/src/main/java/com/socialdemo/socialdemo/SocialdemoApplication.java
@@ -1,1 +1,14 @@
+package com.socialdemo.socialdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialdemoApplication.class, args);
+	}
+
+}
 
