194e9bd01c490fb14e214bc7cae23aa062424aa8
PRITAM RAJ <31183383+rpritam010@users.noreply.github.com>
2021-01-04 10:06:33 +0530

    Update EngineerPrefrenceRepository.java

--- socialdemo/src/main/java/com/socialdemo/socialdemo/repositotry/EngineerPrefrenceRepository.java
+++ socialdemo/src/main/java/com/socialdemo/socialdemo/repositotry/EngineerPrefrenceRepository.java
@@ -12,8 +12,10 @@ import com.socialdemo.socialdemo.entity.EngineerPrefrence;
 @Repository
 public interface EngineerPrefrenceRepository extends JpaRepository<EngineerPrefrence, String> {
 	
-	@Query(value = "SELECT * FROM employee_prefrence t WHERE skill_code IN :skillCode ORDER BY i_prefrence ASC", nativeQuery = true)
-	List<EngineerPrefrence> findByEngineerPrefrence(@Param("skillCode")List<String> skillCode);
-	
+	//@Query(value = "SELECT * FROM employee_prefrence t WHERE skill_code IN :skillCode ORDER BY i_prefrence ASC", nativeQuery = true)
+//	List<EngineerPrefrence> findByEngineerPrefrence(@Param("skillCode")List<String> skillCode);
+
+	@Query(value = "SELECT * FROM employee_prefrence WHERE skill_code = skill_code AND i_prefrence <= 5  ORDER BY i_prefrence ASC", nativeQuery = true)
+	List<EngineerPrefrence> findByEngineerPrefrence(@Param("skill_code")String skill_code);
 
 }
